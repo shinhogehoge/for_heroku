8455b67002f26205bb0e9f3cf1c8f68fbbb2f82a
shin <quantumleap0521@gmail.com>
2019-09-01 13:02:40 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
-$post_body = "hogehoge";
+$post_body = "{\"text\": \"\u307b\u3052\u307b\u3052\" }";
 
-echo $post_body;
+$ch = curl_init("https://hooks.slack.com/services/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");
+
+curl_setopt($ch, CURLOPT_FILE, $post_body);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+
+curl_exec($ch);
+curl_close($ch);
+
+echo $post_body + "をslackに送信します";
 
 ?>
