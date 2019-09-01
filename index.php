48dfc296ea59b7745249d5fc6ff9cf554c2c7be1
shin <quantumleap0521@gmail.com>
2019-09-01 13:08:03 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,25 @@
 <?php
-$post_body = "{\"text\": \"\u307b\u3052\u307b\u3052\" }";
+$post_body = "payload={\"text\": \"\u307b\u3052\u307b\u3052\" }";
 
-$ch = curl_init("https://hooks.slack.com/services/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");
+function send_to_slack($message) {
+  $webhook_url = 'https://hooks.slack.com/services/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';
+  $options = array(
+    'http' => array(
+      'method' => 'POST',
+      'header' => 'Content-Type: application/json',
+      'content' => json_encode($message),
+    )
+  );
+  $response = file_get_contents($webhook_url, false, stream_context_create($options));
+  return $response === 'ok';
+}
 
-curl_setopt($ch, CURLOPT_FILE, $post_body);
-curl_setopt($ch, CURLOPT_HEADER, 0);
+$message = array(
+  'username' => 'phpBot',
+  'text' => 'fooooo;;',
+);
 
-curl_exec($ch);
-curl_close($ch);
+send_to_slack($message);
 
 echo $post_body + "をslackに送信します";
 
